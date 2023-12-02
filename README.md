dcbc028c01036743474b88e6c2b5e3cecca4ad53
Bruce V Wilcox <brucevwilcox@itips.info>
2023-12-02 15:23:16 -0700

    2023-12-02 15:23

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # tax-doc-sdk-tax1098t
 
-Software development kit to generate IRS Form 1098-T as PDF file using Financial Data Exchange (FDX) standards.
+Software development kit to generate IRS Form 1098-T as PDF file using Financial Data Exchange (FDX) standards including QR codes and embedded JSON.
+
+<img alt="Sample Form" src="samples/Tax1098T.sample.png" width="850"/>
 
 This GitHub project is public but the Maven repositories on which it relies are private.
 
--- src/main/java/org/taxdataexchange/usage/tax1098t/Tax1098TDocumentGenerator.java
+++ src/main/java/org/taxdataexchange/usage/tax1098t/Tax1098TDocumentGenerator.java
@@ -56,7 +56,7 @@ public class Tax1098TDocumentGenerator {
         Tax1098TPdfBuilder pdfBuilder = new Tax1098TPdfBuilder( );
         byte[] bytes = pdfBuilder.buildQr( taxDataList );
 
-        String filePath = "samples/Tax1098T.sample.png";
+        String filePath = "samples/Tax1098T.qr.png";
         FileUtils.bytesToFile( bytes, filePath );
         System.out.println( filePath );
 
@@ -84,6 +84,12 @@ public class Tax1098TDocumentGenerator {
         String filePath = "samples/Tax1098T.sample.pdf";
         FileUtils.bytesToFile( pdfBytes, filePath );
         System.out.println( filePath );
+
+        String filePathPng = "samples/Tax1098T.sample.png";
+        byte[] pngBytes = Pdf2PngConverter.convertBytes( pdfBytes );
+        FileUtils.bytesToFile( pngBytes, filePathPng );
+        System.out.println( filePathPng );
+
 
     }
 
